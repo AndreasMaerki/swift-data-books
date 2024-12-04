984ee9be7764bc6b0694b2dac6e2162a7d1eb851
Andreas Maerki <andy1111@me.com>
2024-12-04 09:43:57 +0100

    Initial Commit

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Andreas Maerki on 04.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BooksAppTests/BooksAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppTests.swift
+//  BooksAppTests
+//
+//  Created by Andreas Maerki on 04.12.2024.
+//
+
+import Testing
+@testable import BooksApp
+
+struct BooksAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
