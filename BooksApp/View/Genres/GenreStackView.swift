5a7cb3fcbccfa6df95a981124780af726249c018
Andreas Maerki <andreas.maerki@valora.com>
2025-01-05 17:27:18 +0100

    Update git ignore

---
+++ BooksApp/View/Genres/GenreStackView.swift
@@ -0,0 +1,18 @@
+//
+//  GenreStackView.swift
+//  BooksApp
+//
+//  Created by Andreas Maerki on 05.01.2025.
+//
+
+import SwiftUI
+
+struct GenreStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GenreStackView()
+}
